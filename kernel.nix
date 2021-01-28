65f65f380ec5d4fadc2963df5dc18ebf7d612134
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2021-01-28 17:22:18 -0500

    Clean up kernel, add potential patch

--- kernel.nix
+++ kernel.nix
@@ -1,16 +1,22 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 
 {
-  boot.kernelPatches = [
-    {
-      name = "acso";
-      patch = builtins.fetchurl "https://aur.archlinux.org/cgit/aur.git/plain/add-acs-overrides.patch?h=linux-vfio";
-    }
-  ];
-
-  boot.kernelParams = [
-    "intel_iommu=on"
-    "iommu=pt"
-    "pcie_acs_override=downstream,multifunction"
-  ];
+  boot = {
+    kernelPatches = [
+      {
+        name = "acso";
+        patch = builtins.fetchurl "https://aur.archlinux.org/cgit/aur.git/plain/add-acs-overrides.patch?h=linux-vfio";
+      }
+      # {
+      #   # https://gist.github.com/rupansh/5746cb29b6ce644a37355e4002d22714
+      #   name = "rdtsc-spoof";
+      #   patch = builtins.fetchurl "https://gist.github.com/rupansh/5746cb29b6ce644a37355e4002d22714/raw/efda329359905e7a0a0540b674c087d330492dd2/rdtsc-spoof.patch";
+      # }
+    ];
+    kernelParams = [
+      "intel_iommu=on"
+      "iommu=pt"
+      "pcie_acs_override=downstream,multifunction"
+    ];
+  };
 }
