d2a2bc251f1e0355e37700daa8735bf59b8e2b16
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2021-03-24 12:52:10 -0400

    Update vscode patch script

--- patches/vscode-omnisharp/patch.sh
+++ patches/vscode-omnisharp/patch.sh
@@ -22,15 +22,35 @@ function vsdbgPatch() {
   vsdbg="$csharpDir/.debugger/vsdbg"
   vsdbg_ui="$vsdbg-ui"
 
+  echo "Patching $vsdbg"
   patchelf --set-interpreter $(nb glibc)/lib/ld-linux-x86-64.so.2 --set-rpath $(nb gcc-unwrapped.lib)/lib "$vsdbg"
+
+  echo "Patching $vsdbg_ui"
   patchelf --set-interpreter $(nb glibc)/lib/ld-linux-x86-64.so.2 --set-rpath $(nb gcc-unwrapped.lib)/lib "$vsdbg_ui"
 
+  # Wrap vsdbg
+  vsdbg_patched="$(dirname $vsdbg)/vsdbg_patched"
+  echo "Wrapping $vsdbg -> $vsdbg_patched"
+
+  mv "$vsdbg" "$vsdbg_patched"
+  cat > $vsdbg <<EOF
+#!/usr/bin/env bash
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+export LD_LIBRARY_PATH="$(dirname $vsdbg)"
+$vsdbg_patched "\$@"
+EOF
+  chmod +x "$vsdbg"
+
+  # Wrap vsdbg-ui
   vsdbg_ui_patched="$(dirname $vsdbg)/vsdbg-ui_patched"
+  echo "Wrapping $vsdbg_ui -> $vsdbg_ui_patched"
+
   mv "$vsdbg_ui" "$vsdbg_ui_patched"
   cat > $vsdbg_ui <<EOF
 #!/usr/bin/env bash
-DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1 \
-LD_LIBRARY_PATH=$(dirname $vsdbg_ui):\$(nix-build --no-out-link '<nixpkgs>' -A icu.out)/lib:\$(nix-build --no-out-link '<nixpkgs>' -A openssl.out)/lib $vsdbg_ui_patched
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+export LD_LIBRARY_PATH=$(dirname $vsdbg_ui):\$(nix-build --no-out-link '<nixpkgs>' -A icu.out)/lib:\$(nix-build --no-out-link '<nixpkgs>' -A openssl.out)/lib
+$vsdbg_ui_patched "\$@"
 EOF
   chmod +x "$vsdbg_ui"
 }
