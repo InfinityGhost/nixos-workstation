94d8abbade62b5ca00af615f188939d79861b187
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2021-05-20 22:04:43 -0400

    Use smb for game storage in VMs

--- libvirt.nix
+++ libvirt.nix
@@ -10,4 +10,32 @@
       kmod
     ];
   };
+
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smb_host
+      netbios name = smb_host
+      security = user 
+      hosts allow = 192.168.122.0/24 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      games = {
+        path = "/games";
+        browseable = "yes";
+        "acl allow execute always" = "true";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "infinity";
+        "force group" = "users";
+      };
+    };
+  };
 }
