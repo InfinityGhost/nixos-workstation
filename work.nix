bfab25247998e1b20baaeb233e91ae1c27165b5a
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2021-08-26 22:31:30 -0400

    Update work environment

--- work.nix
+++ work.nix
@@ -1,8 +1,22 @@
 { pkgs, ... }:
 
+let
+  work-rdp = pkgs.writers.writeBashBin "work-rdp" ''
+    ${pkgs.remmina}/bin/remmina -c `${pkgs.coreutils}/bin/readlink -f ~/.local/share/remmina/*work-computer*.remmina`
+  '';
+  work-desktop-rdp = pkgs.makeDesktopItem {
+    name = "work-rdp";
+    desktopName = "Work Computer";
+    exec = "${work-rdp}/bin/work-rdp";
+    icon = "${pkgs.remmina}/share/icons/hicolor/scalable/apps/org.remmina.Remmina.svg";
+    categories = "Network;";
+  };
+in
 {
-  environment.systemPackages = with pkgs; [
+  users.users.infinity.packages = with pkgs; [
     remmina
     teams
+    work-rdp
+    work-desktop-rdp
   ];
 }
