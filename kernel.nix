67662095b9af3657527aba88d3aaa1631b7e0770
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2021-12-24 01:52:34 -0500

    Switch to linux-zen kernel

--- kernel.nix
+++ kernel.nix
@@ -2,16 +2,7 @@
 
 {
   boot = {
-    kernelPackages = pkgs.linuxPackages_5_14;
-    kernelPatches = [
-      {
-        name = "acso";
-        patch = builtins.fetchurl {
-          url = "https://aur.archlinux.org/cgit/aur.git/plain/add-acs-overrides.patch?h=linux-vfio";
-          sha256 = "0xrzb1klz64dnrkjdvifvi0a4xccd87h1486spvn3gjjjsvyf2xr";
-        };
-      }
-    ];
+    kernelPackages = pkgs.linuxPackages_zen;
     extraModprobeConfig = ''
       options kvm ignore_msrs=Y 
       options kvm report_ignored_msrs=N
