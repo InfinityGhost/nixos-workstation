69db482c2b2ebb23ce348da18eac9fab1f53eddd
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2022-01-08 22:05:09 -0500

    Simplify terminal prompt

--- terminal.nix
+++ terminal.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+let
+  termcolor = "%F{12}";
+in
 {
   programs.zsh = {
     enable = true;
@@ -27,24 +30,18 @@
     promptInit = ''
       NEWLINE=''$'\n'
 
-      if [ "$TERM" != "linux" ] && [ "$TERM_PROGRAM" != "vscode" ] && [ -z "''${RIDER_VM_OPTIONS+x}" ]; then
-        # vcs_info Configuration
-        autoload -Uz vcs_info
-        zstyle ':vcs_info:*' check-for-changes true
-        zstyle ':vcs_info:*' formats '%K{12}%F{4}◤%f %b %k%F{12}◤%f'
+      # vcs_info Configuration
+      autoload -Uz vcs_info
 
-        function precmd() {
-          vcs_info
+      zstyle ':vcs_info:*' check-for-changes true
+      zstyle ':vcs_info:*' formats ' ${termcolor}[%b]'
 
-          [ -z ''${vcs_info_msg_0_} ] && eval vcs_info_msg_0_='%k%F{4}◤%f'
-        }
+      function precmd() {
+        vcs_info
+        [ -z "''${vcs_info_msg_0_}" ] && eval vcs_info_msg_0_=' '
+      }
 
-        # Prompt
-        PROMPT='%K{4} %B%n@%m %~ ''${vcs_info_msg_0_}%b''${NEWLINE}%K{4}  %k%F{4}◤%f '
-        PS2='%K{4}  %k  '
-      else
-        PROMPT='%F{4}[%n@%m %~]%f''${NEWLINE}  %# '
-      fi
+      PROMPT='%B${termcolor}%n%f%b@%B${termcolor}%m%b %f%~''${vcs_info_msg_0_}''${NEWLINE}%f  %# '
     '';
   };
   
