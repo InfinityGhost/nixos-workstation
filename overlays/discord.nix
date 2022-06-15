0e083198397627895f918264fbece95424e20cb3
InfinityGhost <infinity@opentabletdriver.net>
2022-06-14 22:22:26 -0400

    Update Discord

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,9 +1,9 @@
 self: super: {
   discord = super.discord.overrideAttrs (old: rec {
-    version = "0.0.17";
+    version = "0.0.18";
     src = super.fetchurl {
       url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-      sha256 = "sha256-NGJzLl1dm7dfkB98pQR3gv4vlldrII6lOMWTuioDExU=";
+      sha256 = "sha256-BBc4n6Q3xuBE13JS3gz/6EcwdOWW57NLp2saOlwOgMI=";
     };
   });
 }
