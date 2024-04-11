f22a4fc6ce381a28122f9ca52355adc02bf54ef8
InfinityGhost <infinity@opentabletdriver.net>
2024-04-11 12:14:15 -0400

    Remove broken pipewire module

--- hosts/nixos-workstation/pipewire.nix
+++ hosts/nixos-workstation/pipewire.nix
@@ -1,21 +1,11 @@
 { inputs, ... }:
 
 {
-  imports = [
-    inputs.nix-gaming.nixosModules.pipewireLowLatency
-  ];
-
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-
-    lowLatency = {
-      enable = true;
-      quantum = 64;
-      rate = 48000;
-    };
   };
 
   # make pipewire realtime-capable
