2e9a67a9c79173c122f37da6d3f13afc41b471bc
InfinityGhost <infinity@opentabletdriver.net>
2024-05-06 20:55:49 -0400

    Add nixos-laptop to hosts

---
+++ hosts/nixos-laptop/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  desktop.gnome.enable = true;
+  desktop.games.enable = true;
+}
